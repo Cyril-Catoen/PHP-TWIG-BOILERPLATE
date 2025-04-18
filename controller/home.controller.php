778063b737f72eff806e32383341f3d957bf2f08
Cyril-Catoen <cyril.catoen@lapiscine.pro>
2025-04-18 14:41:40 +0200

    ajout du controller et de la view

---
+++ controller/home.controller.php
@@ -0,0 +1,33 @@
+<?php 
+
+$articles = [
+    1 => [
+        "id" => 1,
+        "title" => "Article 1",
+        "content" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur tenetur repellendus quod voluptates nemo amet voluptas pariatur, molestias magnam ab cumque quas deserunt, voluptate, nobis iste quae dolores? Debitis, sunt.",
+        "created_at" => new \DateTime(),
+        "image" => "https://cdn.pixabay.com/photo/2018/08/04/11/30/draw-3583548_1280.png",
+        "is_published" => true
+    ],
+
+    2 => [
+        "id" => 2,
+        "title" => "Article 2",
+        "content" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur tenetur repellendus quod voluptates nemo amet voluptas pariatur, molestias magnam ab cumque quas deserunt, voluptate, nobis iste quae dolores? Debitis, sunt.",
+        "created_at" => new \DateTime(),
+        "image" => "https://cdn.pixabay.com/photo/2018/08/04/11/30/draw-3583548_1280.png",
+        "is_published" => false
+    ],
+    3 => [
+        "id" => 3,
+        "title" => "Article 3",
+        "content" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur tenetur repellendus quod voluptates nemo amet voluptas pariatur, molestias magnam ab cumque quas deserunt, voluptate, nobis iste quae dolores? Debitis, sunt.",
+        "created_at" => new \DateTime(),
+        "image" => "https://cdn.pixabay.com/photo/2018/08/04/11/30/draw-3583548_1280.png",
+        "is_published" => true
+    ]
+];
+
+
+require_once('../view/home.view.php');
+?>
